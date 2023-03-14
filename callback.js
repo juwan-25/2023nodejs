a5ec301b3c1d39756ccbb1cbe322458f0ed2777d
Juwan Lee <ljw05w5@gmail.com>
2023-03-15 08:51:18 +0900

    callback 함수 활용하기

---
+++ callback.js
@@ -0,0 +1,12 @@
+//callback 함수
+
+function fuuc_callback() {
+    console.log("콜백함수 실행");
+}
+
+function func_one(callback) {
+    console.log("함수 실행");
+    callback();
+}
+
+func_one(fuuc_callback);
