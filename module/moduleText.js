42093048c1d01444a838b2e93072eded10177fc1
Juwan Lee <ljw05w5@gmail.com>
2023-03-17 09:43:33 +0900

    exports와 module.exports의 참조 관계 알아보기

--- module/moduleText.js
+++ module/moduleText.js
@@ -5,7 +5,17 @@ const text_even = "짝수입니다";
 
 console.log("1 : ");
 
-module.exports = {
-    text_odd,
-    text_even
-}
+// module.exports = {
+//     text_odd,
+//     text_even
+// }
+
+//이렇게 써도 됨
+exports.text_odd = text_odd;
+exports.text_even = text_even;
+
+//이렇게는 안됨
+// exports = {
+//     text_odd,
+//     text_even
+// }
