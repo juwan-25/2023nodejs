50ef1e7993837d21844d5bd40163dfa91513937a
Juwan Lee <ljw05w5@gmail.com>
2023-03-22 10:04:32 +0900

    서버에 파일 읽어서 가져오기

---
+++ server2.js
@@ -0,0 +1,26 @@
+//서버에서 파일 읽어서 가져오기
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res)=>{
+    fs.readFile("./server2_html.html",(err,data)=>{
+        if(err){
+            throw err;
+        }
+        res.statusCode = 200; //성공코드
+        res.setHeader("Content-Type","text/html");
+        res.end(data);
+    });
+});
+
+server.listen(8088);
+
+server.on('listening', ()=>{
+    console.log("8088 포트에서 서버 대기중...");
+});
+
+server.on('error', (error)=>{
+    console.error(error);
+});
+
+
