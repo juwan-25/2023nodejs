cfd25fae43cb75084fbde8a87cb1dfa9272a9f06
Juwan Lee <ljw05w5@gmail.com>
2023-03-29 10:30:39 +0900

    server에서 파일 읽어서 가져오기

---
+++ server/server3.js
@@ -0,0 +1,47 @@
+//서버에서 파일 읽어서 가져오기
+// http://localhost:8088/ -> index 파일
+// http://localhost:8088/index
+// http://localhost:8088/math
+const http = require('http');
+const fs = require('fs').promises;
+const path = require('path');
+
+const server = http.createServer(async (req, res)=>{
+    try{
+        console.log("URL : ",req.url);
+        let fileSet = req.url;
+
+        if(req.url == '/favicon.ico'){
+            //현재는 사용하지 않아서 favicon이 나온다면 오류 발생
+            return res.writeHead(404);
+        } else if(req.url == '/') {
+            //fileSet 아무 입력이 없다면? 기본으로 index.html 호출
+            fileSet = "index";
+        }
+        res.writeHead(200, {"Content-Type":"text/html;charset=utf8"});
+
+        console.log("fileSet : ",fileSet, "__dirname", __dirname);
+        console.log("경로 : ", path.join(__dirname,"./html/",fileSet+".html"));
+
+        //원하는 파일을 열 수 있도록 함
+        const data = await fs.readFile(path.join(__dirname,"./html/",fileSet+".html")); // 절대경로
+        //const data = await fs.readFile(`./html/${fileSet}.html`); //상대경로
+        res.end(data);
+    }catch(err){
+        console.error(err);
+        res.writeHead(200, {"Content-Type":"text/html"});
+        res.end(err.message);
+    }
+});
+
+server.listen(8088);
+
+server.on('listening', ()=>{
+    console.log("8088 포트에서 서버 대기중...");
+});
+
+server.on('error', (error)=>{
+    console.error(error);
+});
+
+
